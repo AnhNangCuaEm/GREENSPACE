3a33084a660746c53d499ac005e8642448646dc4
ANHNANGCUAEM <ths40622@ths.hacl.ac.jp>
2025-01-06 19:00:38 +0900

    env changed to azure mysql server

--- env.php
+++ env.php
@@ -1,6 +1,6 @@
 <?php 
 
 const DB_DATABASE = 'greenspace';
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = 'password';
+const DB_HOST = 'greenspacesmysql.mysql.database.azure.com';
+const DB_USER = 'dbadmin';
+const DB_PASSWORD = 'Greenspace@';
