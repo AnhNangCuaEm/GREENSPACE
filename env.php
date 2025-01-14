885ef0efc75826ee3473e88182a59372eff8753e
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-01-14 12:38:09 +0900

    change to localhost port 3306

--- env.php
+++ env.php
@@ -1,6 +1,6 @@
 <?php 
 
 const DB_DATABASE = 'greenspace';
-const DB_HOST = 'greenspacemysql.mysql.database.azure.com';
-const DB_USER = 'dbadmin';
-const DB_PASSWORD = 'Greenspace@';
+const DB_HOST = 'localhost:3306';
+const DB_USER = 'root';
+const DB_PASSWORD = 'password';
