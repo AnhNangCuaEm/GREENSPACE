61be9aae775549d1560291b38b2db86d1473a834
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-01-21 13:24:15 +0900

    env.php change

--- env.php
+++ env.php
@@ -1,6 +1,7 @@
 <?php 
 
 const DB_DATABASE = 'greenspace';
-const DB_HOST = 'localhost:3306';
-const DB_USER = 'root';
-const DB_PASSWORD = 'password';
+const DB_HOST = 'db-mysql-sgp1-10099-do-user-18598942-0.h.db.ondigitalocean.com';
+const DB_USER = 'doadmin';
+const DB_PASSWORD = 'AVNS_x7lf8MQ73JR2ffBQg9V';
+const DB_PORT = '25060';
