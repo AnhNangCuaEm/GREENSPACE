c5808915b488b73e0ee9da542fb14dbbb9e8d0ce
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-02-18 13:16:32 +0900

    update timezone for PDO connection

--- class/Database.php
+++ class/Database.php
@@ -5,19 +5,15 @@ require_once __DIR__ . '/../env.php';
 if (!class_exists('Database')) {
    class Database
    {
-      private static $connection = null;
-      
       public static function getConnection(): PDO
       {
-         if (self::$connection === null) {
-            self::$connection = new PDO(
-               'mysql:charset=UTF8;dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' . DB_PORT,
-               DB_USER,
-               DB_PASSWORD
-            );
-            self::$connection->exec("SET time_zone = 'Asia/Tokyo'");
-         }
-         return self::$connection;
+         $pdo = new PDO(
+            'mysql:charset=UTF8;dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' . DB_PORT,
+            DB_USER,
+            DB_PASSWORD,
+            [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"]
+         );
+         return $pdo;
       }
    }
 }
