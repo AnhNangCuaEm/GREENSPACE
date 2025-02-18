21e434ee8d8f0a1ae1aa6e6d3c56e2cbff8975ad
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-02-18 12:33:13 +0900

    add timezone to db connection

--- class/Database.php
+++ class/Database.php
@@ -5,13 +5,19 @@ require_once __DIR__ . '/../env.php';
 if (!class_exists('Database')) {
    class Database
    {
+      private static $connection = null;
+      
       public static function getConnection(): PDO
       {
-         return new PDO(
-            'mysql:charset=UTF8;dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' . DB_PORT,
-            DB_USER,
-            DB_PASSWORD
-         );
+         if (self::$connection === null) {
+            self::$connection = new PDO(
+               'mysql:charset=UTF8;dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' . DB_PORT,
+               DB_USER,
+               DB_PASSWORD
+            );
+            self::$connection->exec("SET time_zone = 'Asia/Tokyo'");
+         }
+         return self::$connection;
       }
    }
 }
